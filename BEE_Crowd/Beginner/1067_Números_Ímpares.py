4864ed6b70c6da15ad6cccf974f4b359ce191144
Felipe-Ferreira-web <felipe.ferreira.rodrigues08@gmail.com>
2026-01-18 23:47:28 -0300

    feat: add solution for exercise 1067

---
+++ BEE_Crowd/Beginner/1067_Números_Ímpares.py
@@ -0,0 +1,8 @@
+# Problema: 1067 - Números Ímpares
+# Link: https://judge.beecrowd.com/pt/problems/view/1067
+# Categoria: Beginner
+
+valor = int(input()) + 1
+
+for i in range(1, valor, 2):
+    print(i)
